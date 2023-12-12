6949183b9135b1863a12407b09b0c70df5f8813c
Kim DongHyun <132973383+dancingKimDH@users.noreply.github.com>
2023-12-12 06:05:05 +0000

    Header Layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+export default function Header() {
+    return (
+
+        <div className="header">
+            <div className="header__title"> 귀농귀촌을 위한 할베스트 </div>
+            <div className="header__nav">
+                <ul className="header__nav-items">
+                    <a className="header__nav-items-item">홈페이지</a>
+                    <a className="header__nav-items-item">귀농꿀팁</a>
+                    <a className="header__nav-items-item">커뮤니티</a>
+                    <a className="header__nav-items-item">자유게시판</a>
+                    <a className="header__nav-items-item">나의정보</a>
+                </ul>
+            </div>
+        </div>
+
+
+    )
+}
