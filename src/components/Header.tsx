10e193c028c995f6181497f74dc424f4c8a5943e
Kim DongHyun <132973383+dancingKimDH@users.noreply.github.com>
2023-12-13 02:43:34 +0000

    header update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,18 +9,28 @@ export default function Header() {
             <div className="header__title">
                 <div className="header__title-name">푸른대로</div>
                 <div className="header__title__menu">
-                    <div className="header__title__menu-search"><IoSearch /></div>
-                    <div className="header__title__menu-menu"><MdMenu /></div>
-                    <div className="header__title__menu-signin"><IoMdLogIn /></div>
+                    <div className="header__title__menu-item"><IoSearch /></div>
+                    <div className="header__title__menu-item"><MdMenu /></div>
+                    <div className="header__title__menu-item"><IoMdLogIn /></div>
                 </div>
             </div>
             <div className="header__nav">
                 <ul className="header__nav-items">
-                    <a href="/" className="header__nav-items-item">홈페이지</a>
-                    <a href="/news"  className="header__nav-items-item">귀농뉴스</a>
+                    <div className="header__nav-items-back">
+                        <a href="/" className="header__nav-items-item">홈페이지</a>
+                    </div>
+                    <div className="header__nav-items-back"> 
+                    <a href="/news" className="header__nav-items-item">귀농뉴스</a>
+                    </div>
+                    <div className="header__nav-items-back"> 
                     <a href="/community" className="header__nav-items-item">커뮤니티</a>
+                    </div>
+                    <div className="header__nav-items-back"> 
                     <a className="header__nav-items-item">QnA</a>
+                    </div>
+                    <div className="header__nav-items-back"> 
                     <a href="/mypage" className="header__nav-items-item">마이페이지</a>
+                    </div>
                 </ul>
             </div>
         </div>
