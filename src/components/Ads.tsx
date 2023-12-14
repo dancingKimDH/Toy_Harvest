baa10d4360c065f0208f5c28aa00c505a104fbfe
Kim DongHyun <132973383+dancingKimDH@users.noreply.github.com>
2023-12-14 02:10:12 +0000

    carousel update

--- src/components/Ads.tsx
+++ src/components/Ads.tsx
@@ -1,22 +1,53 @@
 import { useState } from "react";
-import { GrFormPrevious } from "react-icons/gr";
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 import { MdNavigateNext } from "react-icons/md";
 
-export default function Ads () {
+export default function Ads() {
 
-    const[activeIamge, setActiveImage] = useState(1);
+    const [activeImage, setActiveImage] = useState(1);
+    const handleClickNext = () => { setActiveImage(prev => prev + 1 % totalImages) };
+    const handleClickPrev = () => { setActiveImage(prev => (prev - 1 + totalImages) % totalImages) }
+    const totalImages = 3;
 
     return (
         <div className="carousel">
             <ul className="carousel__slides">
-                <li className="carousel__slide-img">
+                <input type="radio" name="radio-buttons" id="img-0" checked={activeImage === 0} readOnly />
+                <li className="carousel__slide-container">
                     <div className="carousel__slide-img">
-                    <img src="" alt="ad1" />
+                        <img src="https://www.pexels.com/photo/white-and-brown-animals-near-fence-158179/" alt="ad1" />
                     </div>
                     <div className="carousel__controls">
-        <label htmlFor="" onClick={() => {setActiveImage(3)}}> <GrFormPrevious/> </label>
+                        <label htmlFor="" onClick={handleClickPrev}> <span> <GrFormPrevious /> </span> </label>
+                        <label htmlFor="" onClick={handleClickNext}> <span> <GrFormNext /> </span> </label>
                     </div>
                 </li>
+                <input type="radio" name="radio-buttons" id="img-1" checked={activeImage === 1} readOnly />
+                <li className="carousel__slide-container">
+                    <div className="carousel__slide-img">
+                        <img src="https://www.pexels.com/photo/white-and-brown-animals-near-fence-158179/" alt="ad1" />
+                    </div>
+                    <div className="carousel__controls">
+                        <label htmlFor="" onClick={handleClickPrev}> <span> <GrFormPrevious /> </span> </label>
+                        <label htmlFor="" onClick={handleClickNext}> <span> <GrFormNext /> </span> </label>
+                    </div>
+                </li>
+                <input type="radio" name="radio-buttons" id="img-2" checked={activeImage === 2} readOnly />
+                <li className="carousel__slide-container">
+                    <div className="carousel__slide-img">
+                        <img src="https://www.pexels.com/photo/white-and-brown-animals-near-fence-158179/" alt="ad1" />
+                    </div>
+                    <div className="carousel__controls">
+                        <label htmlFor="" onClick={handleClickPrev}> <span> <GrFormPrevious /> </span> </label>
+                        <label htmlFor="" onClick={handleClickNext}> <span> <GrFormNext /> </span> </label>
+                    </div>
+                </li>
+                <div className="carousel__dots">
+                    <label htmlFor="" onClick={() => setActiveImage(0)} className="carousel__dot" id="img-dot-0"></label>
+                    <label htmlFor="" onClick={() => setActiveImage(1)} className="carousel__dot" id="img-dot-1"></label>
+                    <label htmlFor="" onClick={() => setActiveImage(2)} className="carousel__dot" id="img-dot-2"></label>
+                </div>
+                
             </ul>
 
         </div>
