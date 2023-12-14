f71985cfbf6ff3b2df290424a3b0feea291e373f
Kim DongHyun <132973383+dancingKimDH@users.noreply.github.com>
2023-12-14 08:36:07 +0000

    carousel jhonna difficult

--- src/components/Ads.tsx
+++ src/components/Ads.tsx
@@ -4,52 +4,46 @@ import { MdNavigateNext } from "react-icons/md";
 
 export default function Ads() {
 
-    const [activeImage, setActiveImage] = useState(1);
-    const handleClickNext = () => { setActiveImage(prev => prev + 1 % totalImages) };
+    const [activeImage, setActiveImage] = useState(0);
+    const handleClickNext = () => { setActiveImage(prev => (prev + 1) % totalImages) };
     const handleClickPrev = () => { setActiveImage(prev => (prev - 1 + totalImages) % totalImages) }
     const totalImages = 3;
 
     return (
+
         <div className="carousel">
             <ul className="carousel__slides">
-                <input type="radio" name="radio-buttons" id="img-0" checked={activeImage === 0} readOnly />
-                <li className="carousel__slides-container">
-                    <div className="image">
-                        <img src="/images/2.jpg" alt="ad1" />
-                    </div>
-                    <div className="carousel__controls">
-                        <label htmlFor="" onClick={handleClickPrev}> <span> <GrFormPrevious /> </span> </label>
-                        <label htmlFor="" onClick={handleClickNext}> <span> <GrFormNext /> </span> </label>
-                    </div>
-                </li>
                 <input type="radio" name="radio-buttons" id="img-1" checked={activeImage === 1} readOnly />
-                <li className="carousel__slides-container">
+                <li className="carousel__slides-container" >
                     <div className="carousel__slides-container-img">
-                        <img src="/images/0.jpg" alt="ad1" />
+                        <img src="/images/4.jpg" alt="image 1" />
                     </div>
-                    <div className="carousel__controls">
-                        <label htmlFor="" onClick={handleClickPrev}> <span> <GrFormPrevious /> </span> </label>
-                        <label htmlFor="" onClick={handleClickNext}> <span> <GrFormNext /> </span> </label>
+                    <div className="carousel__slides-container-controls">
+                        <label htmlFor="" onClick={() => setActiveImage(3)} className="carousel__slides-container-controls-prev"><span><GrFormPrevious /></span></label>
+                        <label htmlFor="" onClick={() => setActiveImage(2)} className="carousel__slides-container-controls-next"><span><GrFormNext /></span></label>
                     </div>
                 </li>
                 <input type="radio" name="radio-buttons" id="img-2" checked={activeImage === 2} readOnly />
-                <li className="carousel__slides-container">
+                <li className="carousel__slides-container" >
                     <div className="carousel__slides-container-img">
-                        <img src="/images/4.jpg" alt="ad1" />
+                        <img src="/images/3.jpg" alt="image 2" />
                     </div>
-                    <div className="carousel__controls">
-                        <label htmlFor="" onClick={handleClickPrev}> <span> <GrFormPrevious /> </span> </label>
-                        <label htmlFor="" onClick={handleClickNext}> <span> <GrFormNext /> </span> </label>
+                    <div className="carousel__slides-container-controls">
+                        <label htmlFor="" onClick={() => setActiveImage(1)} className="carousel__slides-container-controls-prev"><span><GrFormPrevious /></span></label>
+                        <label htmlFor="" onClick={() => setActiveImage(3)} className="carousel__slides-container-controls-next"><span><GrFormNext /></span></label>
                     </div>
                 </li>
-                <div className="carousel__dots">
-                    <label htmlFor="" onClick={() => setActiveImage(0)} className="carousel__dot" id="img-dot-0"></label>
-                    <label htmlFor="" onClick={() => setActiveImage(1)} className="carousel__dot" id="img-dot-1"></label>
-                    <label htmlFor="" onClick={() => setActiveImage(2)} className="carousel__dot" id="img-dot-2"></label>
-                </div>
-                
+                <input type="radio" name="radio-buttons" id="img-3" checked={activeImage === 3} readOnly />
+                <li className="carousel__slides-container" >
+                    <div className="carousel__slides-container-img">
+                        <img src="/images/1.jpg" alt="image 3" />
+                    </div>
+                    <div className="carousel__slides-container-controls">
+                        <label htmlFor="" onClick={() => setActiveImage(2)} className="carousel__slides-container-controls-prev"><span><GrFormPrevious /></span></label>
+                        <label htmlFor="" onClick={() => setActiveImage(1)} className="carousel__slides-container-controls-next"><span><GrFormNext /></span></label>
+                    </div>
+                </li>
             </ul>
-
         </div>
 
     )
