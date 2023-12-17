6224d1399e4600aa6de07cb75888513527f528fe
dancingKimDH <kimdonghyun0701@gmail.com>
2023-12-17 11:44:52 +0900

    signup design

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -18,48 +18,51 @@ export default function SignUpForm() {
     const navigate = useNavigate();
 
     return (
-        <>
-            <div className="button-prev">
-                <button type="button" onClick={() => {navigate(-1)}}><GrFormPrevious /></button>
-            </div>
-            <div className="background-blue">
-                <form action="" onSubmit={onSubmit} className="form__signup">
-                    <h1 className="form__signup__title">
-                        푸른대로와 <span className="text-highlight">함께 시작해 볼까요?</span>
-                    </h1>
-                    <div className="form__signup__block">
-                        <label htmlFor="email"><MdOutlineMail /></label>
-                        <input type="email" name="email" id="email" required onChange={onChange} placeholder="이메일" />
-                    </div>
-                    <div className="form__signup__block">
-                        <label htmlFor="password"><RiLockPasswordFill /></label>
-                        <input type="password" name="password" id="password" required onChange={onChange} placeholder="비밀번호 입력" />
-                    </div>
-                    <div className="form__signup__block">
-                        <label htmlFor="password_confirm"><FaCheck /></label>
-                        <input type="password" name="password_confirm" id="password_confirm" required onChange={onChange} placeholder="비밀번호 확인" />
+        <>                
+        <div className="button-prev">
+            <button type="button" onClick={() => { navigate(-1) }}><GrFormPrevious /></button>
+        </div>
+            <div className="signup__container">
 
-                    </div>
-                    {error && error?.length > 0 && (
+                <div className="background-blue">
+                    <form action="" onSubmit={onSubmit} className="form__signup">
+                        <h1 className="form__signup__title">
+                            푸른대로와 <span className="text-highlight">함께 시작해 볼까요?</span>
+                        </h1>
                         <div className="form__signup__block">
-                            {error}
+                            <label htmlFor="email"><MdOutlineMail /></label>
+                            <input type="email" name="email" id="email" required onChange={onChange} placeholder="이메일" />
                         </div>
-                    )}
+                        <div className="form__signup__block">
+                            <label htmlFor="password"><RiLockPasswordFill /></label>
+                            <input type="password" name="password" id="password" required onChange={onChange} placeholder="비밀번호 입력" />
+                        </div>
+                        <div className="form__signup__block">
+                            <label htmlFor="password_confirm"><FaCheck /></label>
+                            <input type="password" name="password_confirm" id="password_confirm" required onChange={onChange} placeholder="비밀번호 확인" />
 
-                    <div className="form__signup__block">
-                        계정이 있으신가요?
-                        <Link to="/login" className="form__signup__block__link">로그인하기</Link>
-                    </div>
+                        </div>
+                        {error && error?.length > 0 && (
+                            <div className="form__signup__block">
+                                {error}
+                            </div>
+                        )}
 
-                    <div className="form__signup__block">
-                        <input type="button" value="회원가입" className="form__signup__btn-submit" disabled={error?.length > 0} />
-                    </div>
+                        <div className="form__signup__btn">
+                            <input type="button" value="회원가입" className="form__signup__btn-submit" disabled={error?.length > 0} />
+                        </div>
 
-                    <div className="form__signup__block">
-                        <button type="button" className="form__signup__block-google" name="google" onClick={onClickSocialLogIn} ></button>
-                    </div>
+                        <div className="form__signup__block">
+                            <button type="button" className="form__signup__block-google" name="google" onClick={onClickSocialLogIn} ></button>
+                        </div>
 
-                </form>
+                        <div className="form__signup__block-login">
+                            계정이 있으신가요?
+                            <Link to="/login" className="form__signup__block-login-link">로그인하기</Link>
+                        </div>
+
+                    </form>
+                </div>
             </div>
         </>
     )
