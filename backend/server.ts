3bac8aac278db95d92e3922f66365c88f2b1cee2
dancingKimDH <kimdonghyun0701@gmail.com>
2024-03-13 16:21:28 +0900

    backend server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,12 @@
+// node --no-warnings=ExperimentalWarning --loader ts-node/esm file.ts
+
 import express from 'express';
 import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:3000"]
+const allowedOrigins = ["http://localhost:3000", "*"]
 
 const options: cors.CorsOptions = {
     origin: allowedOrigins
@@ -13,7 +16,18 @@ app.use(cors(options));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.json({message: "Data from Backend"})
+})
+
+app.get('/fetch-housing-data', async (req, res) => {
+    try {
+        const response = await axios.get('http://211.237.50.150:7080/openapi/6de97bd2f04693f272abb104a04c73687caad2061a5cbf20eb6f60dd9c4d6719/xml/Grid_20151214000000000336_1/1/5?SLCTN_YEAR=2015');
+        res.json(response.data);
+        return res;
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+        console.log(error);
+    }
 });
 
 app.listen(5000, () => {
--- src/pages/housing/index.tsx
+++ src/pages/housing/index.tsx
@@ -2,15 +2,15 @@ import axios from "axios";
 import Header from "../../components/Utils/Header";
 
 const fetchHousing = async () => {
-    const { data: response } = await axios.get(`http://211.237.50.150:7080/openapi/6de97bd2f04693f272abb104a04c73687caad2061a5cbf20eb6f60dd9c4d6719/xml/Grid_20151214000000000336_1/1/5?SLCTN_YEAR=2015`,
+    const { data: response } = await axios.get('http://localhost:5000/fetch-housing-data',
     {
         headers: {
             'Content-Type': 'application/json',
             'X-Requested-With': 'XMLHttpRequest'
         }
     })
-    const data = JSON.stringify(response);
-    console.log(data)
+    console.log(response);
+    return response;
 }
 
 export default function Housing() {
