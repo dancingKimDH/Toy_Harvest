ba9a5e1fffee4d21cf5eb37c30f11a1defc16ea4
dancingKimDH <kimdonghyun0701@gmail.com>
2024-03-21 16:28:34 +0900

    info table created for the house lease

--- src/pages/housing/index.tsx
+++ src/pages/housing/index.tsx
@@ -1,37 +1,50 @@
 import axios from "axios";
 import Header from "../../components/Utils/Header";
 import convert from "xml-js";
+import { useEffect } from "react";
 
 // import { parseString } from 'xml2js';
 
 
-const fetchHousing = async () => {
-
-    try {
-        const { data: response } = await axios.get('/fetch-housing-data');
-        const result: any = convert.xml2json(response, {
-            compact: true,
-            spaces: 4
-        })
-
-        const jsonResult = JSON.parse(result);
-
-        const rows = jsonResult.Grid_20151214000000000336_1.row;
-        console.log(rows);        
-
-
-    } catch (error) {
-        console.log(error);
-    }
-}
 
 
 export default function Housing() {
+
+    useEffect(
+        () => {
+            const fetchData = async () => {
+                const { data: response } = await axios.get('/fetch-housing-data');
+                const result: any = convert.xml2json(response, {
+                    compact: true,
+                    spaces: 4
+                })
+                const jsonResult = JSON.parse(result);
+                const rows = jsonResult.Grid_20151214000000000336_1.row;
+                console.log(rows);
+            }
+            fetchData();
+        }, []
+    )
+
     return (
         <>
             <Header />
-            <div>
-                <button type="button" onClick={fetchHousing}>FetchHousing</button>            </div>
+            <div className="flex mt-[50px] justify-center">
+                <table className="border border-solid border-black-900 p-5">
+                    <thead className="">
+                        <tr className="">
+                            <th className="p-5">사업 명</th>
+                            <th className="p-5">위치</th>
+                            <th className="p-5">계획 세대 수</th>
+                            <th className="p-5">총 부지 면적(제곱미터)</th>
+                            <th className="p-5">분양공고일</th>
+                            <th className="p-5">담당자 성명</th>
+                            <th className="p-5">담당자 전화번호</th>
+
+                        </tr>
+                    </thead>
+                </table>
+            </div>
         </>
 
     )
