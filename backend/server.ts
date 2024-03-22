dc5e9561fd75d87b4b06f908f73aa466627069a2
dancingKimDH <kimdonghyun0701@gmail.com>
2024-03-22 17:27:32 +0900

    table fetch update finalized

--- backend/server.ts
+++ backend/server.ts
@@ -15,12 +15,13 @@ app.use(cors(options));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({message: "Data from Backend"})
+    res.json({ message: "Data from Backend" })
 })
 
-app.get('/fetch-housing-data', async (req, res) => {
+app.get("/fetch-housing-data/:year", async (req, res) => {
     try {
-        const response = await axios.get(`http://211.237.50.150:7080/openapi/6de97bd2f04693f272abb104a04c73687caad2061a5cbf20eb6f60dd9c4d6719/xml/Grid_20151214000000000336_1/1/5?SLCTN_YEAR=2015`);
+        const { year } = req.params;
+        const response = await axios.get(`http://211.237.50.150:7080/openapi/6de97bd2f04693f272abb104a04c73687caad2061a5cbf20eb6f60dd9c4d6719/xml/Grid_20151214000000000336_1/1/5?SLCTN_YEAR=${year}`);
         // 6de97bd2f04693f272abb104a04c73687caad2061a5cbf20eb6f60dd9c4d6719
         res.json(response.data);
         return res;
--- src/pages/housing/index.tsx
+++ src/pages/housing/index.tsx
@@ -17,11 +17,12 @@ interface RowData {
 export default function Housing() {
 
     const [rowData, setRowData] = useState<RowData[]>([]);
+    const [year, setYear] = useState<Number>(2015);
 
     useEffect(
         () => {
             const fetchData = async () => {
-                const { data: response } = await axios.get('/fetch-housing-data');
+                const { data: response } = await axios.get(`/fetch-housing-data/${year}`);
                 const result: any = convert.xml2json(response, {
                     compact: true,
                     spaces: 4
@@ -32,57 +33,85 @@ export default function Housing() {
                 setRowData(rows);
             }
             fetchData();
-        }, []
+        }, [year]
     )
+
+    const handleYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const { target: { value } } = event;
+        setYear(parseInt(value));
+    }
 
     return (
         <>
             <Header />
-            <div className="flex mt-[50px] justify-center">
-                <table className="border border-solid border-black-900 p-5">
-                    <thead className="">
-                        <tr className="bg-gray-300">
-                            <th className="p-5">사업 명</th>
-                            <th className="p-5">위치</th>
-                            <th className="p-5">계획 세대 수</th>
-                            <th className="p-5">총 부지 면적(제곱미터)</th>
-                            <th className="p-5">분양공고일</th>
-                            <th className="p-5">담당자 성명</th>
-                            <th className="p-5">담당자 전화번호</th>
-                        </tr>
-                    </thead>
+            <div className="">
+                <div className="my-[20px]">
+                    <select onChange={handleYearChange} id="spe_year" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option value="2015" selected>년도 선택</option>
+                        <option value="2005">2005</option>
+                        <option value="2006">2006</option>
+                        <option value="2007">2007</option>
+                        <option value="2008">2008</option>
+                        <option value="2009">2009</option>
+                        <option value="2010">2010</option>
+                        <option value="2011">2011</option>
+                        <option value="2012">2012</option>
+                        <option value="2013">2013</option>
+                        <option value="2014">2014</option>
+                        <option value="2015">2015</option>
+                        <option value="2016">2016</option>
+                        <option value="2017">2017</option>
+                        <option value="2018">2018</option>
+                        <option value="2019">2019</option>
+                        <option value="2020">2020</option>
+                    </select>
+                </div>
+                <div className="flex justify-center mt-[50px]">
+                    <table className="border border-solid border-black-900 p-5">
+                        <thead className="">
+                            <tr className="bg-gray-300">
+                                <th className="p-5">사업 명</th>
+                                <th className="p-5">위치</th>
+                                <th className="p-5">계획 세대 수</th>
+                                <th className="p-5">총 부지 면적(제곱미터)</th>
+                                <th className="p-5">분양공고일</th>
+                                <th className="p-5">담당자 성명</th>
+                                <th className="p-5">담당자 전화번호</th>
+                            </tr>
+                        </thead>
 
-                    {rowData ? (
-                        Object.keys(rowData).map((key: any, index) => (
+                        {rowData ? (
+                            Object.keys(rowData).map((key: any, index) => (
+                                <tbody>
+                                    <tr key={index} className="hover:bg-gray-200 border border-solid border-black-200">
+                                        <td className="p-5 text-center">{rowData[key].BIZ_NM._text}</td>
+                                        <td className="p-5 text-center">{rowData[key].LC_NM._text}</td>
+                                        <td className="p-5 text-center">{rowData[key].PLAN_HSCNT._text}</td>
+                                        <td className="p-5 text-center">{rowData[key].TOT_PLOT_AR._text}</td>
+                                        <td className="p-5 text-center">{rowData[key].LTTOT_PBLANC_DE._text}</td>
+                                        <td className="p-5 text-center">{rowData[key].CHARGER_NM._text}</td>
+                                        <td className="p-5 text-center">{rowData[key].CHARGER_TELNO._text}</td>
+                                    </tr>
+                                </tbody>
+                            ))
+                        ) : (
                             <tbody>
-                                <tr key={index}>
-                                    <td className="p-5 text-center">{rowData[key].BIZ_NM._text}</td>
-                                    <td className="p-5 text-center">{rowData[key].LC_NM._text}</td>
-                                    <td className="p-5 text-center">{rowData[key].PLAN_HSCNT._text}</td>
-                                    <td className="p-5 text-center">{rowData[key].TOT_PLOT_AR._text}</td>
-                                    <td className="p-5 text-center">{rowData[key].LTTOT_PBLANC_DE._text}</td>
-                                    <td className="p-5 text-center">{rowData[key].CHARGER_NM._text}</td>
-                                    <td className="p-5 text-center">{rowData[key].CHARGER_TELNO._text}</td>
+                                <tr key="no-data">
+                                    <td colSpan={7}>데이터가 존재하지 않습니다</td>
                                 </tr>
                             </tbody>
-                        ))
-                    ) : (
+                        )}
+
+
+
+
+
+
                         <tbody>
-                            <tr key="no-data">
-                                <td colSpan={7}>데이터가 존재하지 않습니다</td>
-                            </tr>
+
                         </tbody>
-                    )}
-
-
-
-
-
-
-                    <tbody>
-
-                    </tbody>
-                </table >
+                    </table >
+                </div>
             </div >
         </>
 
