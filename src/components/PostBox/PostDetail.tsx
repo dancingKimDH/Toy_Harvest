2537ca073a7b6d9f34694d6259087407be9b7299
dancingKimDH <kimdonghyun0701@gmail.com>
2024-03-26 16:13:51 +0900

    post delete

--- src/components/PostBox/PostDetail.tsx
+++ src/components/PostBox/PostDetail.tsx
@@ -1,13 +1,46 @@
 import { User } from "firebase/auth";
+import { deleteDoc, doc } from "firebase/firestore";
 import { PostProps } from "interface";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { toast } from "react-toastify";
+import { useNavigate, useParams } from "react-router-dom";
+import { db, storage } from "firebaseApp";
+import { deleteObject, ref } from "firebase/storage";
 
 interface PostDetailProp {
     post: PostProps;
     user: User | null;
 }
 
-export default function PostDetail({post, user}: PostDetailProp) {
-    
+export default function PostDetail({ post, user }: PostDetailProp) {
+
+    const navigate = useNavigate();
+
+    const { id } = useParams();
+
+    const handleDelete = async () => {
+        const confirm = window.confirm("게시글을 삭제하시겠습니까?");
+        if (confirm && id) {
+            try {
+                const imageRef = ref(storage, post?.imageUrl);
+                if (post?.imageUrl) {
+                    deleteObject(imageRef).catch((error) => {
+                        console.log(error);
+                    })
+                }
+                await deleteDoc(doc(db, "posts", id));
+                toast.success("성공적으로 삭제하였습니다");
+                await navigate("/community");
+            } catch (error) {
+                toast.error("삭제 중 오류가 발생하였습니다")
+                console.log(error);
+            }
+
+        } else {
+            toast.warn("삭제를 취소하였습니다");
+        }
+    }
+
     return (
         <div className="px-5">
             {post ? (
@@ -15,6 +48,7 @@ export default function PostDetail({post, user}: PostDetailProp) {
                     <div className="mx-auto lg:w[1600px] md:w-[1000px] w-[300px] box-border">
                         <div className="flex left-0 right-0 mt-8 px-2 sm:px-0">
                             <h3 className="w-full bg-gray-100 rounded-lg p-3 text-lg underline font-semibold leading-7 text-gray-900">{post?.title}</h3>
+                            <button className="p-4" type="button" onClick={handleDelete}><RiDeleteBin5Line /></button>
                         </div>
                         <div className="px-4 py-6">
                             {post?.imageUrl && post?.imageUrl.length > 0 && (
@@ -34,7 +68,7 @@ export default function PostDetail({post, user}: PostDetailProp) {
                                         <dt className="text-sm font-medium leading-6 text-gray-900">해시태그</dt>
                                         <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
                                             {post?.hashTags?.map((hashTag, index) => (
-                                                <span className="text-blue-500 font-semibold">#{hashTag}{' '}</span>
+                                                <span key={index} className="text-blue-500 font-semibold">#{hashTag}{' '}</span>
                                             ))}</dd>
                                     </div>
                                     <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
--- src/pages/community/details.tsx
+++ src/pages/community/details.tsx
@@ -10,61 +10,6 @@ import { toast } from "react-toastify";
 
 export default function CommunityPostDetail() {
 
-    const people = [
-        {
-            name: 'Leslie Alexander',
-            email: 'leslie.alexander@example.com',
-            role: 'Co-Founder / CEO',
-            imageUrl:
-                'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            lastSeen: '3h ago',
-            lastSeenDateTime: '2023-01-23T13:23Z',
-        },
-        {
-            name: 'Michael Foster',
-            email: 'michael.foster@example.com',
-            role: 'Co-Founder / CTO',
-            imageUrl:
-                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            lastSeen: '3h ago',
-            lastSeenDateTime: '2023-01-23T13:23Z',
-        },
-        {
-            name: 'Dries Vincent',
-            email: 'dries.vincent@example.com',
-            role: 'Business Relations',
-            imageUrl:
-                'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            lastSeen: null,
-        },
-        {
-            name: 'Lindsay Walton',
-            email: 'lindsay.walton@example.com',
-            role: 'Front-end Developer',
-            imageUrl:
-                'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            lastSeen: '3h ago',
-            lastSeenDateTime: '2023-01-23T13:23Z',
-        },
-        {
-            name: 'Courtney Henry',
-            email: 'courtney.henry@example.com',
-            role: 'Designer',
-            imageUrl:
-                'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            lastSeen: '3h ago',
-            lastSeenDateTime: '2023-01-23T13:23Z',
-        },
-        {
-            name: 'Tom Cook',
-            email: 'tom.cook@example.com',
-            role: 'Director of Product',
-            imageUrl:
-                'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            lastSeen: null,
-        },
-    ]
-
     const { id } = useParams();
     const { user } = useContext(AuthContext);
     const [post, setPost] = useState<PostProps | null>(null);
@@ -78,10 +23,6 @@ export default function CommunityPostDetail() {
             setPost({ ...(docSnap.data() as PostProps) });
         }
     }
-
-    useEffect(() => {
-        getPost();
-    }, [user, id])
 
     const handleCommentSubmit = async (e: any) => {
         e.preventDefault();
@@ -102,7 +43,8 @@ export default function CommunityPostDetail() {
                 await updateDoc(postRef, {
                     comments: arrayUnion(commentObj)
                 })
-
+                setCommentValue("");
+                fetchPostAfterComment();
                 toast.success("댓글을 성공적으로 등록하였습니다");
 
             } catch (error) {
@@ -110,6 +52,14 @@ export default function CommunityPostDetail() {
                 console.log(error);
             }
         }
+    }
+
+    useEffect(() => {
+        getPost();
+    }, [user, id])
+
+    const fetchPostAfterComment = async () => {
+        await getPost();
     }
 
     const commentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -124,7 +74,7 @@ export default function CommunityPostDetail() {
             <Header />
             <div className="px-5">
                 {post ? (
-                    <PostDetail post={post} user={user} />
+                    <PostDetail post={post} user={user}/>
                 ) : (
                     <div>웹페이지가 존재하지 않습니다</div>
                 )}
