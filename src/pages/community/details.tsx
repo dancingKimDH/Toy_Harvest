88a5f0b361a695f6fd5a486ebcc3efca1c0f249b
dancingKimDH <kimdonghyun0701@gmail.com>
2024-03-26 13:44:42 +0900

    post details update

--- src/pages/community/details.tsx
+++ src/pages/community/details.tsx
@@ -25,83 +25,55 @@ export default function CommunityPostDetail() {
         getPost();
     }, [user, id])
 
-    console.log(post);
-    console.log(user);
-
     return (
         <>
             <Header />
             <div className="px-5">
                 {post ? (
                     <div className="flex justify-center">
-                        <div className="mx-5 px-3 md:w-[1600px] w-[800px]">
-                            <div className="mt-8 px-4 sm:px-0">
-                                <h3 className="bg-gray-200 rounded-lg p-3 text-lg shaow-gray-300 underline font-semibold leading-7 text-gray-900">{post?.title}</h3>
+                        <div className="mx-auto lg:w[1600px] md:w-[1000px] w-[300px] box-border">
+                            <div className="flex left-0 right-0 mt-8 px-2 sm:px-0">
+                                <h3 className="w-full bg-gray-200 rounded-lg p-3 text-lg shaow-gray-300 underline font-semibold leading-7 text-gray-900">{post?.title}</h3>
                             </div>
-                            <div className="mt-3 border-t border-gray-300">
-                                <dl className="divide-y divide-gray-100">
-                                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt className="text-sm font-medium leading-6 text-gray-900">작성 날짜</dt>
-                                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{post?.createdAt}</dd>
+                            <div className="px-4 py-6">
+                                {post?.imageUrl && (
+                                    <div className="flex justify-center">
+                                        <img className="object-cover" src={post?.imageUrl} alt="이미지" />
                                     </div>
-                                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt className="text-sm font-medium leading-6 text-gray-900">해시태그</dt>
-                                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                            {post?.hashTags?.map((hashTag, index) => (
-                                                <span className="text-blue-500 font-semibold">#{hashTag}{' '}</span>
-                                            ))}</dd>
-                                    </div>
-                                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt className="text-sm font-medium leading-6 text-gray-900">작성자</dt>
-                                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                            {user?.displayName ? user?.displayName : '익명'}</dd>
-                                    </div>
-                                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt className="text-sm font-medium leading-6 text-gray-900">이메일 주소</dt>
-                                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{post?.email}</dd>
-                                    </div>
-                                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt className="text-sm font-medium leading-6 text-gray-900">작성 글</dt>
-                                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                            {post?.content}
-                                        </dd>
-                                    </div>
-                                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt className="text-sm font-medium leading-6 text-gray-900">Attachments</dt>
-                                        <dd className="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                                            <ul role="list" className="divide-y divide-gray-100 rounded-md border border-gray-200">
-                                                <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                                                    <div className="flex w-0 flex-1 items-center">
+                                )}
+                            </div>
+                            <div className="flex justify-center">
+                                <div className="grow mt-3 border-t border-gray-300">
+                                    <dl className="divide-y divide-gray-100">
+                                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                            <dt className="text-sm font-medium leading-6 text-gray-900">작성 날짜</dt>
+                                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{post?.createdAt}</dd>
+                                        </div>
+                                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                            <dt className="text-sm font-medium leading-6 text-gray-900">해시태그</dt>
+                                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                                {post?.hashTags?.map((hashTag, index) => (
+                                                    <span className="text-blue-500 font-semibold">#{hashTag}{' '}</span>
+                                                ))}</dd>
+                                        </div>
+                                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                            <dt className="text-sm font-medium leading-6 text-gray-900">작성자</dt>
+                                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                                {user?.displayName ? user?.displayName : '익명'}</dd>
+                                        </div>
+                                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                            <dt className="text-sm font-medium leading-6 text-gray-900">이메일 주소</dt>
+                                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{post?.email}</dd>
+                                        </div>
+                                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                            <dt className="text-sm font-medium leading-6 text-gray-900">작성 글</dt>
+                                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                                {post?.content}
+                                            </dd>
+                                        </div>
 
-                                                        <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                                                            <span className="truncate font-medium">resume_back_end_developer.pdf</span>
-                                                            <span className="flex-shrink-0 text-gray-400">2.4mb</span>
-                                                        </div>
-                                                    </div>
-                                                    <div className="ml-4 flex-shrink-0">
-                                                        <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                                            Download
-                                                        </a>
-                                                    </div>
-                                                </li>
-                                                <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                                                    <div className="flex w-0 flex-1 items-center">
-
-                                                        <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                                                            <span className="truncate font-medium">coverletter_back_end_developer.pdf</span>
-                                                            <span className="flex-shrink-0 text-gray-400">4.5mb</span>
-                                                        </div>
-                                                    </div>
-                                                    <div className="ml-4 flex-shrink-0">
-                                                        <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                                            Download
-                                                        </a>
-                                                    </div>
-                                                </li>
-                                            </ul>
-                                        </dd>
-                                    </div>
-                                </dl>
+                                    </dl>
+                                </div>
                             </div>
                         </div>
                     </div>
