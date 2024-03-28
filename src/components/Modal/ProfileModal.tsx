52d1be8c1f18083a1fd1f2c0ebce5ff2979cfd55
dancingKimDH <kimdonghyun0701@gmail.com>
2024-03-28 16:31:38 +0900

    modal update

--- src/components/Modal/ProfileModal.tsx
+++ src/components/Modal/ProfileModal.tsx
@@ -9,19 +9,19 @@ interface ProfileModalProps {
     setModal: React.Dispatch<React.SetStateAction<string>>
 }
 
-export default function ProfileModal ({name, email, createdAt, modal, setModal}: ProfileModalProps) {
-    
-    return(
-        <div className="">
-            <div className="">
-                <img className="" src="" alt="" />
-            </div>
-            <div>
-                <p>{name}</p>
-                <p>{email}</p>
-                <p>{createdAt}</p>
-            </div>
+export default function ProfileModal({ name, email, createdAt, modal, setModal }: ProfileModalProps) {
+
+    const isOpen = !!modal;
+
+    return (
+        <Modal isOpen={isOpen} onRequestClose={() => setModal("")}>
+            <h1>프로필</h1>
+            <h2>{name}</h2>
+            <h3>{email}</h3>
+            <h3>{createdAt}</h3>
             <button type="button" onClick={() => setModal("")}>Close</button>
-        </div>
+        </ Modal>
+
+
     )
 }
