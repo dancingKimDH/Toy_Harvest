d8c5d3efdcf85d8df6e003a75128ecf42ca9caf9
dancingKimDH <kimdonghyun0701@gmail.com>
2024-03-30 16:23:12 +0900

    modal style update

--- src/components/Modal/ProfileModal.tsx
+++ src/components/Modal/ProfileModal.tsx
@@ -1,5 +1,5 @@
-import Modal from "react-modal";
 import { useEffect, useState } from "react";
+import ReactModal from "react-modal";
 
 interface ProfileModalProps {
     postUid: string,
@@ -25,8 +25,15 @@ export default function ProfileModal({ postUid, modal, setModal }: ProfileModalP
     const isOpen = !!modal;
     console.log(postUid);
 
+    const modalStyle = {
+        overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.1)'
+        }
+    }
+
     return ( 
-        <Modal appElement={document.getElementById('root') || undefined} className="rounded-lg absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 bg-white w-[350px] h-[350px] lg:w-[400px] lg:h-[400px] flex justify-center" isOpen={isOpen} onRequestClose={() => setModal("")}>
+        <ReactModal  appElement={document.getElementById('modal') || undefined} className="rounded-lg absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 bg-white w-[350px] h-[350px] lg:w-[400px] lg:h-[400px] flex justify-center" 
+        isOpen={isOpen} onRequestClose={() => setModal("")} ariaHideApp={false} shouldCloseOnEsc={true} style={modalStyle}>
             <div className="flex flex-col items-center">
                 <img className="lg:m-[30px] mt-[25px] mb-[10px] w-[150px] h-[150px] rounded-full" src="/images/2.jpg" alt="" />
                 <h2 className="lg:text-[30px] text-center text-lg font-semibold">{name}</h2>
@@ -34,7 +41,7 @@ export default function ProfileModal({ postUid, modal, setModal }: ProfileModalP
                 <h3 className="lg:text-[20px] mb-3 text-center text-gray-600">{createdAt}</h3>
             </div>
             <button className="lg:text-[20px] hover:bg-blue-700 text-sm text-white bg-primaryBlue p-3 mx-auto fixed text-center rounded-lg w-full bottom-0" type="button" onClick={() => setModal("")}>닫기</button>
-        </ Modal>
+        </ ReactModal>
 
 
     )
