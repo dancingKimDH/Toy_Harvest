997e21f4308c25e1abe97d6e990fec79db58e7aa
dancingKimDH <kimdonghyun0701@gmail.com>
2024-04-23 13:26:26 +0900

    update

--- src/components/intro/intro_community.tsx
+++ src/components/intro/intro_community.tsx
@@ -4,11 +4,90 @@ import Ads from "components/Utils/Ads";
 
 import { FullPage, Slide } from "react-full-page";
 
+import PropTypes from 'prop-types';
+
+interface CustomControlsProps {
+    className?: string;
+    getCurrentSlideIndex: () => number;
+    onNext: () => void;
+    onPrev: () => void;
+    scrollToSlide: (index: number) => void;
+    slidesCount: number;
+    style?: React.CSSProperties;
+    buttonClassName?: string;
+}
+
+interface CustomControlsProps {
+    className?: string;
+    getCurrentSlideIndex: () => number;
+    onNext: () => void;
+    onPrev: () => void;
+    scrollToSlide: (index: number) => void;
+    slidesCount: number;
+    style?: React.CSSProperties;
+}
+
+const CustomControls: React.FC<CustomControlsProps> = ({
+    className = 'full-page-controls',
+    getCurrentSlideIndex,
+    onNext,
+    onPrev,
+    scrollToSlide,
+    slidesCount,
+    style = {
+        left: '50%',
+        paddingTop: '10px',
+        position: 'fixed',
+        transform: 'translateX(-50%)',
+    },
+    buttonClassName = "custom-button"
+}) => {
+    const renderSlidesNumbers = (currentSlideIndex: number) => {
+        const slidesNumbers = [];
+        for (let i = 0; i < slidesCount; i++) {
+            const buttonProps = {
+                disabled: currentSlideIndex === i,
+                key: i,
+                onClick: () => scrollToSlide(i),
+                className: buttonClassName,
+            };
+            slidesNumbers.push(<button type="button" {...buttonProps}>{i + 1}</button>);
+        }
+        return slidesNumbers;
+    };
+
+    const currentSlideIndex = getCurrentSlideIndex();
+
+    return (
+        <div className={className} style={style}>
+            <button
+                type="button"
+                disabled={currentSlideIndex === 0}
+                onClick={onPrev}
+                className={buttonClassName}
+            >
+                previous
+            </button>
+            {renderSlidesNumbers(currentSlideIndex)}
+            <button
+                type="button"
+                disabled={currentSlideIndex === slidesCount - 1}
+                onClick={onNext}
+                className={buttonClassName}
+            >
+                next
+            </button>
+        </div>
+    );
+};
+
 export default class IntroCommunity extends Component {
+
     render() {
         return (
             <div className="w-full h-full">
-                <FullPage controls controlsProps={{className: "slide-navigation"}}>
+                <FullPage controls={CustomControls}>
+                    {/*  controlsProps={{className: "slide-navigation"}} */}
                     <Slide>
                         <Ads />
                     </Slide>
