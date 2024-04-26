abf1cb66f8dab05ea84000e3f35aa1b1158e1d6d
dancingKimDH <kimdonghyun0701@gmail.com>
2024-04-26 17:26:44 +0900

    swiper pagination

--- src/components/intro/intro_community.tsx
+++ src/components/intro/intro_community.tsx
@@ -8,8 +8,10 @@ import { GoArrowUp, GoArrowDown } from "react-icons/go";
 import { IoRadioButtonOff } from "react-icons/io5";
 
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
 
 import "swiper/css";
+import "swiper/css/pagination";
 
 interface CustomControlsProps {
     className?: string;
@@ -142,7 +144,7 @@ export default class IntroCommunity extends Component {
                         </div>
                     </Slide>
                     <Slide>
-                        <Swiper loop={true} autoplay={{delay: 2500, disableOnInteraction: false}} navigation pagination={{clickable: true}} scrollbar={{draggable: true}}>
+                        <Swiper modules={[Pagination]} loop={true} autoplay={{ delay: 2500, disableOnInteraction: false }} pagination={{clickable: true}} scrollbar={{ draggable: true }}>
                             <SwiperSlide>
                                 <div className="onepage_container flex flex-col">
                                     <div className="flex justify-end">
@@ -159,14 +161,14 @@ export default class IntroCommunity extends Component {
                                 </div>
                             </SwiperSlide>
                             <SwiperSlide>
-                                    <div className="onepage_container flex flex-col">
-                                        Hello World!
-                                    </div>
+                                <div className="onepage_container flex flex-col">
+                                    Hello World!
+                                </div>
                             </SwiperSlide>
                             <SwiperSlide>
-                                    <div className="onepage_container flex flex-col">
-                                        Third page
-                                    </div>
+                                <div className="onepage_container flex flex-col">
+                                    Third page
+                                </div>
                             </SwiperSlide>
                         </Swiper>
                     </Slide>
