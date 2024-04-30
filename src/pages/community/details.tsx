dc9266ad8ae11e720e16bd45feb3b4011b6c1b1e
dancingKimDH <kimdonghyun0701@gmail.com>
2024-04-30 19:24:20 +0900

    comment css update

--- src/pages/community/details.tsx
+++ src/pages/community/details.tsx
@@ -74,13 +74,13 @@ export default function CommunityPostDetail() {
             <Header />
             <div className="px-5">
                 {post ? (
-                    <PostDetail post={post} user={user}/>
+                    <PostDetail post={post} user={user} />
                 ) : (
                     <div>웹페이지가 존재하지 않습니다</div>
                 )}
 
             </div>
-            <PostComment post={post}/>
+            <PostComment post={post} />
             <div className="mt-5 mx-auto lg:w[1600px] md:w-[1000px] w-[300px] box-border">
                 <form action="" onSubmit={handleCommentSubmit}>
                     <div className="py-2 bg-primaryBrown rounded-lg px-2 border-b-2 border-solid border-primaryGrey">
@@ -90,6 +90,21 @@ export default function CommunityPostDetail() {
                         <textarea name="comment" onChange={commentChange} className="w-full outline-none" placeholder="의견을 남겨보세요" value={commentValue}></textarea>
                     </div>
                     <button className="hover:bg-gray-300 rounded-lg bg-gray-100 p-2 w-full my-6" type="submit">작성하기</button>
+                </form>
+                <form>
+                    <label htmlFor="chat" className="sr-only">Your message</label>
+                    <div className="flex items-center py-4 rounded-lg bg-gray-50 dark:bg-gray-700">
+                        <div className="w-full">
+                            <span className="text-white font-semibold p-4">안녕하세요 </span>
+                            <textarea id="chat" className="resize-none block ml-2 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
+                        </div>
+                        <button type="submit" className="ml-2 inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
+                            <svg className="w-5 h-5 rotate-90 rtl:-rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                                <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
+                            </svg>
+                            <span className="sr-only">Send message</span>
+                        </button>
+                    </div>
                 </form>
             </div>
         </>
--- src/pages/profile/details.tsx
+++ src/pages/profile/details.tsx
@@ -1,40 +1,45 @@
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Fragment, useContext } from 'react'
 import Header from 'components/Utils/Header'
+import AuthContext from 'context/AuthContext';
+import { getAuth } from 'firebase/auth';
+import { app } from 'firebaseApp';
+import { MdOutlineEmail } from 'react-icons/md';
+import { IoMdPerson } from 'react-icons/io';
 
 export default function ProfileDetail() {
 
-  const user = {
-    name: 'Tom Cook',
-    email: 'tom@example.com',
-    imageUrl:
-      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  }
-  const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
-    { name: 'Reports', href: '#', current: false },
-  ]
-  const userNavigation = [
-    { name: 'Your Profile', href: '#' },
-    { name: 'Settings', href: '#' },
-    { name: 'Sign out', href: '#' },
-  ]
+  const {user} = useContext(AuthContext);
+  const auth = getAuth(app);
 
-  function classNames(...classes: string[]) {
-    return classes.filter(Boolean).join(' ')
-  }
+  console.log(user?.email);
+
+  
 
   return (
 
     <>
       <Header />
-      <div className='w-full h-full absolute top-[108px] lg:top-[180px] bg-black'>
-        <div className='text-black w-full bg-primaryBlue p-4 mt-5'>
-          마이 페이지
+      <div className='w-full h-full absolute top-[100px] lg:top-[160px] bg-black'>
+        <div className='flex items-center justify-between text-black rounded-lg font-semibold w-[80%] max-w-[600px] mx-auto mt-9 bg-primaryBlue p-4'>
+          <span className='text-[1.5rem]'>마이 페이지</span>
+          <span className='underline hover:text-gray-500 hover:cursor-pointer'>로그아웃</span>
+        </div>
+        <div className='bg-white rounded-lg mt-9 w-[90%] max-w-[800px] mx-auto p-4'>
+            <span className='font-semibold underline'>나의 정보</span>
+            <div className='mt-3 flex items-center justify-between'>
+              <span className=''><MdOutlineEmail /></span>
+              <textarea name="" id="" className='w-[80%] border border-black resize-none'>{user?.email}</textarea>
+            </div>
+            <div className='mt-3 flex items-center justify-between'>
+              <span className=''><IoMdPerson /></span>
+              <span>{user?.displayName}</span>
+            </div>
+        </div>
+        <div className='bg-white rounded-lg mt-9 w-[90%] max-w-[800px] mx-auto p-4'>
+          <span className='font-semibold underline'>
+            내가 쓴 글
+          </span>
+
         </div>
       </div>
     </>
